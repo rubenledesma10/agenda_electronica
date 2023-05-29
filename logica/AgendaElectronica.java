b7b8ef12fe3b710906a3a0c2046f9d99e75470e0
rubenledesma10 <122803942+rubenledesma10@users.noreply.github.com>
2023-05-29 20:28:29 -0300

    Add files via upload

---
+++ logica/AgendaElectronica.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author ledes
+ */
+public class AgendaElectronica {
+
+    public static void main(String[] args) {
+         Principal princ = new Principal();
+         princ.setVisible(true); //para que la pantalla se haga visible
+         princ.setLocationRelativeTo(null); //para poner la pantalla en el medio
+    }
+    
+}
